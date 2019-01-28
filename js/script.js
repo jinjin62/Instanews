dba3865c954f551050bdde395fe19ea33b8e61dd
Gavin Jin <gavinjin62@gmail.com>
2019-01-28 10:37:27 -0800

    Implemented mobile first

--- js/script.js
+++ js/script.js
@@ -1,7 +1,3 @@
-$(function() {
-  console.log("Welcome to Instanews!");
-});
-
 // Problem: Retrieve content from the NYT Top Stories API and add it to our site.
 // If we dont get a successful response, let the user know;
 
@@ -14,32 +10,70 @@ $(function() {
 //5. If unsuccessful, show a helpful error message to the user in the UI
 //6. hide the loader again
 
-// same as document ready
-$(function() {
-  $("#section").on("change", function() {
-    //selecting the dropdown menu 1a
-    const section = $(this).val();
+$(".storygrid").hide();
+// $(".loadinggif").hide();
 
-    // if sectopm is empty, return
-    // show loader
-    // clear stories
+$("#options").on("change", function() {
+  $(".storygrid").empty();
+  $("#heading" /*header*/).attr("class", "top");
+  $(".storygrid")
+    .delay(200)
+    .fadeIn();
+  $(".loading").show();
 
-    //https://api.nytimes.com/svc/topstories/v2/{section}.json
-
-    //make out ajax request
-    $.ajax({
-      method: "GET",
-      url:
-        "https://api.nytimes.com/svc/topstories/v2/" +
-        section +
-        ".json?api-key=7GASR12G1Xh0u1vVwK7R35tHubffWv5y",
-      dataType: "json"
-    }).done(function(data){
-      //append all the things most code will be in here
-    }).fail(function()  {
-
-   }).always(function() {
-    //hid tthe loader
-    });
-  });
+  var val = $(this).val();
+  loadContent(val);
 });
+
+function loadContent(value) {
+  var url = "https://api.nytimes.com/svc/topstories/v2/" + value + ".json";
+  url +=
+    "?" +
+    $.param({
+      "api-key": "7GASR12G1Xh0u1vVwK7R35tHubffWv5y"
+    });
+
+  $.ajax({
+    method: "GET",
+    url: url
+  })
+    .done(function(data) {
+      var i = 0;
+      while (i <= 11) {
+        var stories = "";
+
+        // console.log(data.results[i].multimedia);
+
+        if (data.results[i].multimedia.length > 0) {
+          stories +=
+            '<a target="_blank" href="' +
+            data.results[i].url +
+            '" class= "tiles">';
+          stories += '<img src="';
+          stories += data.results[i].multimedia[4].url;
+          stories += '"/>';
+          stories += '<p> "';
+          stories += data.results[i].abstract;
+          stories += '"</p>';
+          stories += "</a>";
+
+          $(".storygrid").append(stories);
+
+          //   console.log(data.results);
+        }
+
+        i++;
+      } // endwhile
+    })
+
+    .fail(function(error) {
+      throw error;
+    })
+    .always(function() {
+      $(".loading")
+        .hide()
+        .delay(2000);
+    });
+}
+
+// API key =
